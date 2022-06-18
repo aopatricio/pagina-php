12e005d29e23a1d2afa438ea5d5c19f016b78c28
Alessandro Patricio <33489774+aopatricio@users.noreply.github.com>
2022-06-18 19:51:56 -0300

    Update index.php

--- index.php
+++ index.php
@@ -16,6 +16,7 @@ echo 'User IP Address - '.$_SERVER['REMOTE_ADDR'];
 <p>This is a normal paragraph.</p>
 <p class="ex1">Openshift - Ambiente 200DEV.</p>
 <p class="ex2">Funcionou PORRA.</p>
+<p class="ex2">Funcionou Na VM da 200DEV.</p>
 
 </body>
 </html>
