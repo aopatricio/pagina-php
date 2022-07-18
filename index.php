bc880e431d331c2f5fb24e5c442293ef255378a7
Alessandro Patricio <33489774+aopatricio@users.noreply.github.com>
2022-07-18 20:43:53 -0300

    Update index.php
    Adicionando test do flux

--- index.php
+++ index.php
@@ -16,6 +16,7 @@ echo 'User IP Address of PODs - '.$_SERVER['REMOTE_ADDR'];
 <p class="ex1">Openshift - Ambiente 200DEV.</p>
 <p class="ex2">Openshift 4.10</p>
 <p class="ex2">Funcionou Na VM da 200DEV.</p>
+<p class="ex2">Adicionando o Flux Na VM da 200DEV.</p>
 
 </body>
 </html>
